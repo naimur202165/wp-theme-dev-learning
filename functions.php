d6c96a26b5b5edd8ff09615cad6f0b37d29bcc89
naimur <naimur202165@gmail.com>
2025-02-19 21:52:01 +0600

    all thing is updated

--- functions.php
+++ functions.php
@@ -17,6 +17,10 @@ function  naimur_css_js_file_calling()
     wp_register_style('custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0', 'all');
     wp_enqueue_style('bootstrap');
     wp_enqueue_style('custom');
+    //jQuery
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '5.0.2', 'true');
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', 'true');
 }
 
 add_action('wp_enqueue_scripts', 'naimur_css_js_file_calling');
--- index.php
+++ index.php
@@ -1,25 +1,33 @@
-  <?php
+<?php
+/*
+* This template for displaying the header
+*/
+?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
 
-  // This Template is for displaying header  
-  ?>
+<head>
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
 
-  <!DOCTYPE html>
-  <html lang=" <?php language_attributes(); ?>" class="no-js">
+<body <?php body_class(); ?>>
+  <div id="header_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <a href=""><img src="<?php echo get_theme_mod('ali_logo'); ?>" alt=""></a>
+        </div>
+        <div class="col-md-9"></div>
+      </div>
+    </div>
+  </div>
 
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Wp Head -->
-    <?php wp_head(); ?>
-  </head>
-  <!-- Wp Body -->
-
-  <body <?php body_class(); ?>>
 
 
-    <!-- wp-body -->
-    <!-- To SHow -->
-    <?php wp_footer(); ?>
-  </body>
+  <?php wp_footer(); ?>
+</body>
 
-  </html>
+</html>
