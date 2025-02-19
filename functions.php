b209fae38292f7fad08bf89ab046d36bbe870599
naimur <naimur202165@gmail.com>
2025-02-19 21:47:46 +0600

    custom file is loaded

--- functions.php
+++ functions.php
@@ -12,6 +12,11 @@ add_theme_support('title-tag');
 function  naimur_css_js_file_calling()
 {
     wp_enqueue_style('naimur-style', get_stylesheet());
+    // Registaring  the custom style
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '5.0.2', 'all');
+    wp_register_style('custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('custom');
 }
 
 add_action('wp_enqueue_scripts', 'naimur_css_js_file_calling');
