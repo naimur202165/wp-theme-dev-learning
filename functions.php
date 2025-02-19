40d19d51385542106dab34b4c4c2d3e5cd4d1efd
naimur <naimur202165@gmail.com>
2025-02-19 21:37:13 +0600

    naimur stylesheet is addedg

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
 
-
 // My Theme Function
 
 
 
-// Theme Title
-
-
+// Theme Titlle
 add_theme_support('title-tag');
+
+
+// Theme CSS and jQuery File calling
+function  naimur_css_js_file_calling()
+{
+    wp_enqueue_style('naimur-style', get_stylesheet());
+}
+
+add_action('wp_enqueue_scripts', 'naimur_css_js_file_calling');
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
   <?php
 
-    // This Template is for displaying header  
-    ?>
+  // This Template is for displaying header  
+  ?>
 
   <!DOCTYPE html>
   <html lang=" <?php language_attributes(); ?>" class="no-js">
 
   <head>
-      <meta charset="<?php bloginfo('charset'); ?>">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- Wp Head -->
-      <?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Wp Head -->
+    <?php wp_head(); ?>
   </head>
   <!-- Wp Body -->
 
   <body <?php body_class(); ?>>
 
 
-      <!-- wp-body -->
-      <!-- To SHow -->
-      <?php wp_footer(); ?>
+    <!-- wp-body -->
+    <!-- To SHow -->
+    <?php wp_footer(); ?>
   </body>
 
   </html>
